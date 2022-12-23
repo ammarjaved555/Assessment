51ea681f23d700cdca77ffd6aafeda5e7b67dba3
ammarjaved555 <ammarjaved555@gmail.com>
2022-12-23 10:55:49 +0500

    className change

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,15 +2,15 @@ import React from "react";
 import '../App.css';
 const SideBar = () => {
   return (
-    <div class="sideBar">
-    <ul class="sidebar">
-    <i class="fa fa-light fa-house"></i>
-    <i class="fa  fa-duoton fa-memo-circle-info"></i>
+    <div className="sideBar">
+    <ul className="sidebar">
+    <i className="fa fa-light fa-house"></i>
+    <i className="fa  fa-duoton fa-memo-circle-info"></i>
     
-    <i class="fa fa-duotone fa-shop"></i>
-    <i class="fa fa-duotone fa-headphones"></i>
-    <i class="fa fa-duotone fa-box-open"></i>
-    <i class="fa fa-duotone fa-bell"></i>
+    <i className="fa fa-duotone fa-shop"></i>
+    <i className="fa fa-duotone fa-headphones"></i>
+    <i className="fa fa-duotone fa-box-open"></i>
+    <i className="fa fa-duotone fa-bell"></i>
     </ul>
 </div>
 
